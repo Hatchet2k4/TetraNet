300bfd75e3b3daed503231d7eb3faa61f955ad92
Francis Brazeau <hatchet2k4@gmail.com>
2024-02-27 14:22:58 -0500

    Add HoldGrid class.

---
+++ src/HoldGrid.cs
@@ -0,0 +1,44 @@
+namespace TetraNet;
+
+using Godot;
+
+
+public partial class HoldGrid : TextureRect
+{
+
+	[Export] private TextureRect _holdGrid;
+	public Block _holdBlock;
+
+	public override void _Ready()
+	{
+		SetProcess(false);
+	}
+
+	public bool HasBlock()
+	{
+		return _holdBlock != null;
+	}
+
+	public void ClearBlock()
+	{
+		_holdBlock = null;
+	}
+
+	public void SetBlock(Block block)
+	{
+		_holdBlock = block;
+	}
+
+	public Block GetBlock()
+	{
+		return _holdBlock;
+	}
+
+	public Block SwapBlock(Block block)
+	{
+		Block returnBlock = _holdBlock;
+		_holdBlock = block;
+		return returnBlock;
+	}
+
+}
