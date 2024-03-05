99670c639ac414b815b7d7f3c4ea6068aea939ad
Francis Brazeau <hatchet2k4@gmail.com>
2024-03-05 09:44:33 -0500

    Start main menu.

--- src/TextEntry.cs
+++ src/TextEntry.cs
@@ -7,13 +7,7 @@ public partial class TextEntry : TextEdit
 	[Export] private NinePatchRect _highlight;
 	[Export] private MainField _mainField;
 	[Export] private TextEdit _chatBox;
-
 	[Export] private AudioStreamPlayer _chatSound;
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
 
 	public void OnGuiInput(InputEvent @event)
 	{
@@ -23,30 +17,6 @@ public partial class TextEntry : TextEdit
 			Text = "";
 			_chatSound.Play();
 		}
-
-		/*
-		if (@event is InputEventMouseButton mouseEvent && mouseEvent.ButtonIndex == MouseButton.Left && mouseEvent.Pressed)
-		{
-			if (WithinBounds(mouseEvent.Position))
-			{
-				GD.Print("Clicked!");
-				GrabFocus();
-				_highlight.Visible = true;
-				
-			}
-			else
-			{
-				GD.Print("Back to main");
-				_highlight.Visible = false;
-				_mainField.GrabFocus();
-			}
-		}
-		*/
-	}
-
-	public bool WithinBounds(Vector2 pos)
-	{
-		return pos.X >= Position.X && pos.X < Position.X + Size.X && pos.Y >= Position.Y && pos.Y < Position.Y + Size.Y;
 	}
 
 	public void OnFocusEntered()
