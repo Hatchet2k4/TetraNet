26ff0c4de2defd7b8c221a2619149e72c6dedccd
Francis Brazeau <hatchet2k4@gmail.com>
2024-03-06 19:56:06 -0500

    Add port, chat box to host menu

--- src/HostMenu.cs
+++ src/HostMenu.cs
@@ -6,7 +6,9 @@ public partial class HostMenu : Control
 {
 
 	[Export] public OptionButton teamOption;
+	[Export] public LineEdit portBox;
 
+	private int port;
 
 	public override void _Ready()
 	{
