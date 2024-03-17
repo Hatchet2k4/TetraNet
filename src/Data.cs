b66be1b01998bbe0059b8db563cff0b9d6928518
Francis Brazeau <hatchet2k4@gmail.com>
2024-03-17 15:55:52 -0400

    Assign item textures for inventory

--- src/Data.cs
+++ src/Data.cs
@@ -16,22 +16,25 @@ public partial class Data : Node
 	public static readonly Vector2 LEFT = Vector2.Left;
 	public static readonly Vector2 RIGHT = Vector2.Right;
 
-	public static readonly int NUM_ITEMS = 9;
+	public static readonly int NUM_ITEMS = 12;
 
 	public enum ItemType
 	{
-		A, //add random line
-		B, //remove special blocks
+		//defense
 		C, //clear line
+		D, //defense shield - block attacks/line adds for X seconds
 		G, //gravity
-		N, //nuke
+		N, //nuke field - clear all blocks		
+
+		//attack
+		A, //add random line
+		B, //remove special blocks	
+		L, //lock rotation
+		H, //lock hold button			
 		O, //explode special blocks
 		Q, //quake
 		R, //random 
 		S //switch field
-		  //Z, //increase fall speed
-		  //L, //lock rotation
-		  //H, //lock hold button
 	}
 
 	public static Dictionary<int, string> TeamMappings = new()
--- src/Inventory.cs
+++ src/Inventory.cs
@@ -10,14 +10,17 @@ public partial class Inventory : NinePatchRect
 
 	public override void _Ready()
 	{
-		for (int x = 0; x < 4; x++)
+		int index = 0;
+		for (int y = 0; y < 3; y++)
 		{
-			for (int y = 0; y < 3; y++)
+			for (int x = 0; x < 4; x++)
 			{
 				Item itm = _itemScene.Instantiate() as Item;
 				AddChild(itm);
 				itm.Position = new Vector2(20 + x * 120, 24 + y * 72);
 				itm.TooltipText = "Test Tooltip";
+				itm.Texture = _main.itemTextures[index];
+				index++;
 			}
 		}
 	}
